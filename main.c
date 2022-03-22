dfa654dc22d8723265beb8dd4c95bdb7f66c6aee
neosarchizo <neosarchizo@gmail.com>
2022-03-22 15:56:57 +0900

    feat: add toggle functions

--- main.c
+++ main.c
@@ -7,16 +7,37 @@
 #define TEST_LED_PORT GPIOB
 #define TEST_LED_PIN GPIO_PIN_5
 
+void toggleByWrite(void)
+{
+	GPIO_Write(TEST_LED_PORT, 1 << 5);
+	delay_ms(1000);
+	GPIO_Write(TEST_LED_PORT, 0);
+	delay_ms(1000);
+}
+
+void toggleByHighLow(void)
+{
+	GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
+	delay_ms(1000);
+	GPIO_WriteLow(TEST_LED_PORT, TEST_LED_PIN);
+	delay_ms(1000);
+}
+
+void toggleByReverse(void)
+{
+	GPIO_WriteReverse(TEST_LED_PORT, TEST_LED_PIN);
+	delay_ms(1000);
+}
+
 int main()
 {
 	CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
 	CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
 
-	GPIO_Init(TEST_LED_PORT, TEST_LED_PIN, GPIO_MODE_OUT_PP_LOW_FAST);
-	
+	GPIO_Init(TEST_LED_PORT, TEST_LED_PIN, GPIO_MODE_OUT_OD_LOW_SLOW);
+
 	while (1)
 	{
-		GPIO_WriteReverse(TEST_LED_PORT, TEST_LED_PIN);
-		delay_ms(1000);
+		toggleByReverse();
 	}
 }
