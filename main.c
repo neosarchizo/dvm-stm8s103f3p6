d727b39e58ea72680f608fdfc491d225d998925d
neosarchizo <neosarchizo@gmail.com>
2022-03-24 15:42:21 +0900

    feat: add digital read example

--- main.c
+++ main.c
@@ -7,26 +7,33 @@
 #define TEST_LED_PORT GPIOB
 #define TEST_LED_PIN GPIO_PIN_5
 
-void toggleByWrite(void)
-{
-	GPIO_Write(TEST_LED_PORT, 1 << 5);
-	delay_ms(1000);
-	GPIO_Write(TEST_LED_PORT, 0);
-	delay_ms(1000);
+#define SW_PORT GPIOC
+#define SW_PIN GPIO_PIN_3
+
+void readByPin(void){
+	uint8_t value = GPIO_ReadInputPin(SW_PORT, SW_PIN);
+
+	if (value)
+	{
+		// turn off led
+		GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
+	} else {
+		// turn on led
+		GPIO_WriteLow(TEST_LED_PORT, TEST_LED_PIN);
+	}
 }
 
-void toggleByHighLow(void)
-{
-	GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
-	delay_ms(1000);
-	GPIO_WriteLow(TEST_LED_PORT, TEST_LED_PIN);
-	delay_ms(1000);
-}
+void readByPort(void) {
+	uint8_t value = GPIO_ReadInputData(SW_PORT);
 
-void toggleByReverse(void)
-{
-	GPIO_WriteReverse(TEST_LED_PORT, TEST_LED_PIN);
-	delay_ms(1000);
+	if (value & (1 << 3))
+	{
+		// turn off led
+		GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
+	} else {
+		// turn on led
+		GPIO_WriteLow(TEST_LED_PORT, TEST_LED_PIN);
+	}
 }
 
 int main()
@@ -35,9 +42,12 @@ int main()
 	CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
 
 	GPIO_Init(TEST_LED_PORT, TEST_LED_PIN, GPIO_MODE_OUT_OD_LOW_SLOW);
+	GPIO_Init(SW_PORT, SW_PIN, GPIO_MODE_IN_PU_NO_IT);
+
+	// uint8_t value = GPIO_ReadOutputData(TEST_LED_PORT);
 
 	while (1)
 	{
-		toggleByReverse();
+		readByPort();
 	}
 }
