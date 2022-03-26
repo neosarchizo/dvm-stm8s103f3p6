b21ca7649475add458574a4bacd3a99c29d942cf
neosarchizo <neosarchizo@gmail.com>
2022-03-26 18:19:34 +0900

    feat: add pwm

--- main.c
+++ main.c
@@ -1,30 +1,16 @@
 #include <stm8s.h>
 #include <stm8s_clk.h>
 #include <stm8s_gpio.h>
-#include <stm8s_exti.h>
+#include <stm8s_tim2.h>
 #include <stdint.h>
 #include <delay.h>
 
-#define TEST_LED_PORT GPIOB
-#define TEST_LED_PIN GPIO_PIN_5
-
-#define SW_PORT GPIOC
-#define SW_PIN GPIO_PIN_3
-
-void PC_IRQHandler(void) __interrupt(5)
+static void TIM2_Config(void)
 {
-	uint8_t value = GPIO_ReadInputPin(SW_PORT, SW_PIN);
+	TIM2_TimeBaseInit(TIM2_PRESCALER_1, 256);
 
-	if (value)
-	{
-		// turn off led
-		GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
-	}
-	else
-	{
-		// turn on led
-		GPIO_WriteLow(TEST_LED_PORT, TEST_LED_PIN);
-	}
+	TIM2_OC3Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 0, TIM2_OCPOLARITY_HIGH);
+	TIM2_Cmd(ENABLE);
 }
 
 int main()
@@ -32,15 +18,13 @@ int main()
 	CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
 	CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
 
-	GPIO_Init(TEST_LED_PORT, TEST_LED_PIN, GPIO_MODE_OUT_OD_LOW_SLOW);
-	GPIO_WriteHigh(TEST_LED_PORT, TEST_LED_PIN);
+	TIM2_Config();
 
-	GPIO_Init(SW_PORT, SW_PIN, GPIO_MODE_IN_PU_IT);
-	EXTI_SetExtIntSensitivity(EXTI_PORT_GPIOC, EXTI_SENSITIVITY_RISE_FALL);
-
-	enableInterrupts();
+	uint8_t i = 0;
 
 	while (1)
 	{
+		TIM2_SetCompare3(i++);
+		delay_ms(20);
 	}
 }
