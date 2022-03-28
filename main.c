e3af3824700c45fe4b915cddca9ed003d3344a1c
neosarchizo <neosarchizo@gmail.com>
2022-03-28 15:11:59 +0900

    feat: add uart

--- main.c
+++ main.c
@@ -1,42 +1,31 @@
 #include <stm8s.h>
 #include <stm8s_clk.h>
 #include <stm8s_gpio.h>
-#include <stm8s_tim2.h>
-#include <stm8s_adc1.h>
+#include <stm8s_uart1.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <delay.h>
 
-static void TIM2_Config(void)
+static void UART1_Config(void)
 {
-	TIM2_TimeBaseInit(TIM2_PRESCALER_1, 1024);
-
-	TIM2_OC3Init(TIM2_OCMODE_PWM1, TIM2_OUTPUTSTATE_ENABLE, 0, TIM2_OCPOLARITY_HIGH);
-	TIM2_Cmd(ENABLE);
+	UART1_Init(
+		115200,
+		UART1_WORDLENGTH_8D,
+		UART1_STOPBITS_1,
+		UART1_PARITY_NO,
+		UART1_SYNCMODE_CLOCK_DISABLE,
+		UART1_MODE_TXRX_ENABLE
+	);
+	UART1_Cmd(ENABLE);
 }
 
-uint16_t ADC_Read(uint8_t channel)
+int putchar(int c)
 {
-	ADC1_Init(
-		ADC1_CONVERSIONMODE_SINGLE,
-		channel,
-		ADC1_PRESSEL_FCPU_D2,
-		ADC1_EXTTRIG_TIM,
-		DISABLE,
-		ADC1_ALIGN_RIGHT,
-		channel,
-		DISABLE);
-	ADC1_Cmd(ENABLE);
-
-	ADC1_StartConversion();
-	
-	while (RESET == ADC1_GetFlagStatus(ADC1_FLAG_EOC))
+	UART1_SendData8(c);
+	while (UART1_GetFlagStatus(UART1_FLAG_TXE) == RESET)
 	{
 	}
-	ADC1_ClearFlag(ADC1_FLAG_EOC);
-
-	uint16_t result = ADC1_GetConversionValue();
-	ADC1_DeInit();
-	return result;
+	return c;
 }
 
 int main()
@@ -44,11 +33,16 @@ int main()
 	CLK_SYSCLKConfig(CLK_PRESCALER_CPUDIV1);
 	CLK_SYSCLKConfig(CLK_PRESCALER_HSIDIV1);
 
-	TIM2_Config();
+	UART1_Config();
+
+	printf("Hello world!\n");
 
 	while (1)
 	{
-		uint16_t valAdc = ADC_Read(ADC1_CHANNEL_3);
-		TIM2_SetCompare3(valAdc);
+		if (UART1_GetFlagStatus(UART1_FLAG_RXNE) == SET)
+		{
+			int c = UART1_ReceiveData8();
+			putchar(c);
+		}
 	}
 }
